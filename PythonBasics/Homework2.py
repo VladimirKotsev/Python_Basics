5f48843a4f4a9692952f32684ef2ceefb8981405
Vladimir Kotsev <v.kotsev04@gmail.com>
2024-10-24 11:19:41 +0300

    Upload solution for homework 2. Upload unit tests

--- PythonBasics/Homework2.py
+++ PythonBasics/Homework2.py
@@ -6,22 +6,34 @@ def get_count_unique_letters_for_keys(**kwargs):
 
     return len(unique_letters)
 
+valid_bush_names = ['храст', 'bush', 'shrub']
+
 def function_that_says_ni(*args, **kwargs):
-
     cost = 0
+    has_named = False
+
+    # execute for kwargs(named)
     count = get_count_unique_letters_for_keys(**kwargs)
-    print(len(args))
-    print(len(kwargs))
 
-    if len(args) == 0:
-        #execute for named arguments
+    for name, dictionary in kwargs.items():
+        if 'name' in dictionary and dictionary.get('name').lower() in valid_bush_names: #Good bush
+            cost += dictionary.get('cost', 0)
+            has_named = True
 
-        for key, value in args:
-            print(f'{key} -> {value}')
-    else:
-        #execute for one dictionary
-        for key in args:
-            print(f'{key} -> {args[key]}')
+    # execute for args(positional)
+    for arg in args:
+        if type(arg) is not dict:
+            continue
 
+        dictionary = dict(arg)
 
-function_that_says_ni(ab={"name": "храст", "cost": 1.80}, bc={"name": "храст", "cost": 1}, c ={"name": "не съм храст", "cost": 3})
+        if 'name' in dictionary and dictionary['name'].lower() in valid_bush_names:
+            cost += dictionary.get('cost', 0)
+
+    if cost == 0 or cost > 42:
+        return 'Ni!'
+    if has_named and count % (cost // 1) != 0:
+        return 'Ni!'
+
+    return f'{cost:.2f}лв'
+
